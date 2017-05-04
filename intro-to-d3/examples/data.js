1eecc7a3c55416b942daeeabef64d9945ecdcae2
Nicholas Frota <info@nicholasfrota.com>
2017-05-03 21:24:20 -0400

    aaa

--- intro-to-d3/examples/data.js
+++
@@ -1,7 +0,0 @@
-var numbers = [ 5, 4, 10, 1 ],
-    data = [
-      { date: '2014-01-01', amount: 10 },
-      { date: '2014-02-01', amount: 20 },
-      { date: '2014-03-01', amount: 40 },
-      { date: '2014-04-01', amount: 80 }
-    ];
